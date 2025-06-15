588d58006bc07cd9168938dafb4e304e7c82f960
Alexander759 <aleks.asenov2005@gmail.com>
2025-06-15 16:45:54 +0300

    Add prohject information

--- Spreadsheets/main.cpp
+++ Spreadsheets/main.cpp
@@ -1,3 +1,14 @@
+/**
+* Solution to course project #2 
+* OOP Course
+* Faculty of Mathematics and Informatics of Sofia University
+* Summer semester 2024/2025
+* 
+* @author Alexander Asenov
+* @idnumber 2MI0600422
+* @compiler VCC
+*/
+
 #include <iostream>
 #include "TableConfigure.h"
 #include "Position.h"
